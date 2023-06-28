806efc2accb2ccb90e4738cf64dc13c4f892ac73
Fabian Michael <hallo@fabianmichael.de>
2023-06-28 11:20:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 
-use Kirby\Cms\App as Kirby;
+use Kirby\Cms\App;
 
 @require_once __DIR__ . '/helpers.php';
 
-Kirby::plugin('fabianmichael/cachebuster', [
+App::plugin('fabianmichael/cachebuster', [
 	'options' => [
 		'active' => true,
 	],
 	'components' => [
-		'css' => fn (Kirby $kirby, string $url, $options = null) => cachebuster_versionize($kirby, $url, $options),
-		'js' => fn (Kirby $kirby, string $url, $options = null) => cachebuster_versionize($kirby, $url, $options),
+		'css' => fn (App $kirby, string $url, $options = null) => cachebuster_versionize($kirby, $url, $options),
+		'js' => fn (App $kirby, string $url, $options = null) => cachebuster_versionize($kirby, $url, $options),
 	],
 ]);
